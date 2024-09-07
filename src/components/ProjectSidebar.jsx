9491d9a98d954239d0469bb2d4bbb33e0eaaa6ac
the3musketeersgh <the3musketeersgh@gmail.com>
2024-09-07 02:16:57 +0000

    initial commit, added projectsidebar

---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ProjectSidebar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <button> + Add Project</button>
+      <ul></ul>
+    </aside>
+  );
+}
+
+export default ProjectSidebar;
