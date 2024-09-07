a5559355c36cb75719e7d783bc76816feb65e171
the3musketeersgh <the3musketeersgh@gmail.com>
2024-09-07 03:42:10 +0000

    added noprojectselected and button components

--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "./Button";
 
 function ProjectSidebar() {
   return (
@@ -6,10 +7,7 @@ function ProjectSidebar() {
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
-      <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
-        {" "}
-        + Add Project
-      </button>
+      <Button>+ Add Project</Button>
       <ul></ul>
     </aside>
   );
