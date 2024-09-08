f9ebf3663025fae3713931c8781c3baabb3c35f2
the3musketeersgh <the3musketeersgh@gmail.com>
2024-09-08 14:05:27 +0000

    final commit: added code to clear/delete task

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,14 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {}
+  function handleDeleteTask(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
 
   function handleSelectProject(id) {
     //b
@@ -108,6 +115,7 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
         onSelectedProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -16,7 +16,10 @@ function Tasks({ tasks, onAdd, onDelete }) {
           {tasks.map((task) => (
             <li key={task.id} className="flex justify-between my-4">
               <span>{task.text}</span>
-              <button className="text-stone-700 hover:text-red-500">
+              <button
+                onClick={() => onDelete(task.id)}
+                className="text-stone-700 hover:text-red-500"
+              >
                 Clear
               </button>
             </li>
